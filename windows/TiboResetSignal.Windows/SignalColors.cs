612c71b288a2b4018b4c11380c558fa32bc71d50
zoro <zoro@mail.vault.netvoyage.com>
2026-09-03 09:28:19 +0900

    fix: disambiguate Windows signal color type

--- windows/TiboResetSignal.Windows/SignalColors.cs
+++ windows/TiboResetSignal.Windows/SignalColors.cs
@@ -1,14 +1,14 @@
-using System.Windows.Media;
+using MediaColor = System.Windows.Media.Color;
 
 namespace TiboResetSignal.WinApp;
 
 internal static class SignalColors
 {
-    internal static Color For(SignalLevel level) => level switch
+    internal static MediaColor For(SignalLevel level) => level switch
     {
-        SignalLevel.Red => Color.FromRgb(235, 91, 91),
-        SignalLevel.Yellow => Color.FromRgb(236, 174, 73),
-        SignalLevel.Green => Color.FromRgb(91, 214, 150),
-        _ => Color.FromRgb(115, 115, 122)
+        SignalLevel.Red => MediaColor.FromRgb(235, 91, 91),
+        SignalLevel.Yellow => MediaColor.FromRgb(236, 174, 73),
+        SignalLevel.Green => MediaColor.FromRgb(91, 214, 150),
+        _ => MediaColor.FromRgb(115, 115, 122)
     };
 }
